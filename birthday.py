52ab223c187c65b5d54a34148a0060698199332b
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-02-05 14:15:32 -0500

    Updated from Brython Server: 2/5/2016 2:15:32 PM

--- birthday.py
+++ birthday.py
@@ -1,6 +1,6 @@
 """
 birthday.py
-Author: <your name here>
+Author: Billy B
 Credit: <list sources used, if any>
 Assignment:
 
@@ -31,3 +31,4 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+input("Hello, what is yout name?")
